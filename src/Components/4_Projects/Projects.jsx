83e6a3e033f943572145e7c1ce9d2719474de990
Jag Jeevan Ricky <avengr7890@gmail.com>
2025-05-28 20:45:01 +0530

    Refactor code structure for improved readability and maintainability

--- src/Components/4_Projects/Projects.jsx
+++ src/Components/4_Projects/Projects.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ProjectCard from "./ProjectCard";
 
-import Ima1 from "../0_Assets/avatar.jpeg";
-import Ima2 from "../0_Assets/Image 1.jpeg";
-import Ima3 from "../0_Assets/Imafe.jpeg";
+import SS1 from "../0_Assets/SS1.png";
+import SS2 from "../0_Assets/SS2.png";
+import SS3 from "../0_Assets/SS3.png";
+import SS4 from "../0_Assets/SS4.png";
 
 const Projects = () => {
   return (
@@ -17,24 +18,31 @@ const Projects = () => {
       <div className="flex justify-center items-center text-center my-12 gap-12 flex-wrap">
         <ProjectCard
           
-          Image={Ima1}
+          Image={SS1}
           Alt="ToDo_Lists"
           link="https://g-1-todo-list.vercel.app/"
           Heading="To-Do Lists"
           Para="A web application to add tasks for your day to day life."
         />
         <ProjectCard
-          Image={Ima2}
+          Image={SS2}
           Alt="Expense_Tracker"
           link="https://g-1-exp-tracker.vercel.app/" 
           Heading="Expense Tracker"
           Para="A web application to calculate your expenses."
         />
         <ProjectCard
-          Image={Ima3}
+          Image={SS3}
           Alt="Portfolio"
           link="https://portfolio2jagjeevan.netlify.app"
           Heading="Portfolio"
+          Para="A UI design for my Portfolio page to showcase my design skills."
+        />
+        <ProjectCard
+          Image={SS4}
+          Alt="Weather_App"
+          link="https://g-1weatherapp.vercel.app/"
+          Heading="Weather App"
           Para="A UI design for my Portfolio page to showcase my design skills."
         />
       </div>
